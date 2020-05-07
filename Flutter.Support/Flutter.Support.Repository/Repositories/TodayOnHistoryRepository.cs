5bf7800d1e54bfca0fe30cc8f55f1962fec7d2a2
liuqi <liuqi@tongtongmall.com>
2020-05-07 17:42:46 +0800

    *)

---
+++ Flutter.Support/Flutter.Support.Repository/Repositories/TodayOnHistoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Flutter.Support.Domain.IRepositories;
+using Flutter.Support.SqlSugar;
+using Flutter.Support.SqlSugar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flutter.Support.Repository.Repositories
+{
+    public class TodayOnHistoryRepository : SugarDbContext<TodayOnHistory>, ITodayOnHistoryRepository
+    {
+        public TodayOnHistoryDetail Detail(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodayOnHistory> Query(string day)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
